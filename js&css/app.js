ce7e686841e38709adae6a817b46e4b55b8f8353
adzizPe <jebangkit26@gmail.com>
2024-07-20 00:09:13 +0700

    spinner loading screen

--- js&css/app.js
+++ js&css/app.js
@@ -1,5 +1,4 @@
-// app.ts
-// Penggunaan fungsi di dalam event listener jika diperlukan
+// js
 $(document).ready(function () {
     // Contoh penggunaan fungsi pada event
     $(".navbar-brand").click(function () {
@@ -7,14 +6,12 @@ $(document).ready(function () {
         var panjang = hitungPanjangString(nama);
         console.log("Panjang nama: ".concat(panjang));
     });
-    // Pengaturan smooth scrolling
     $("a").on('click', function (event) {
         var _a, _b, _c;
         // Kode smooth scrolling
         var target = $(this).attr("href");
         if (target && target !== "") {
             event.preventDefault();
-            // Menggunakan conditional operator untuk menangani tipe target
             var offsetTop = (_c = (_b = (_a = $(target)) === null || _a === void 0 ? void 0 : _a.offset()) === null || _b === void 0 ? void 0 : _b.top) !== null && _c !== void 0 ? _c : 0;
             $('html, body').animate({
                 scrollTop: offsetTop
@@ -24,20 +21,23 @@ $(document).ready(function () {
         }
     });
 });
-// Event listener untuk klik pada nama
 $(".navbar-brand").on('click', function (event) {
     var _a, _b, _c;
-    // Misalnya, untuk navigasi ke bagian about pada halaman
     var target = "#about";
-    // Menggunakan animasi smooth scrolling ke bagian about
     var offsetTop = (_c = (_b = (_a = $(target)) === null || _a === void 0 ? void 0 : _a.offset()) === null || _b === void 0 ? void 0 : _b.top) !== null && _c !== void 0 ? _c : 0;
     $('html, body').animate({
         scrollTop: offsetTop
     }, 800);
-    // Tambahkan tindakan lainnya di sini
     alert("Anda mengklik nama Saya dan Anda akan diarahkan ke bagian About!");
 });
-// Contoh fungsi untuk menghitung panjang string
 function hitungPanjangString(input) {
     return input.length;
 }
+window.addEventListener('load', function () {
+    setTimeout(function () {
+        var loadingScreen = document.getElementById('loading-screen');
+        if (loadingScreen) {
+            loadingScreen.style.display = 'none';
+        }
+    }, 2000);
+});
--- js&css/app.ts
+++ js&css/app.ts
@@ -1,6 +1,6 @@
-// app.ts
+// app.ts --> ke javascript
 
-// Penggunaan fungsi di dalam event listener jika diperlukan
+
 $(document).ready(function() {
     // Contoh penggunaan fungsi pada event
     $(".navbar-brand").click(function() {
@@ -9,13 +9,13 @@ $(document).ready(function() {
         console.log(`Panjang nama: ${panjang}`);
     });
     
-    // Pengaturan smooth scrolling
+    
     $("a").on('click', function(event) {
         // Kode smooth scrolling
         let target = $(this).attr("href");
         if (target && target !== "") {
             event.preventDefault();
-             // Menggunakan conditional operator untuk menangani tipe target
+             
              let offsetTop = $(target)?.offset()?.top ?? 0;
             $('html, body').animate({
                 scrollTop: offsetTop
@@ -25,22 +25,32 @@ $(document).ready(function() {
         }
     });
 });
-// Event listener untuk klik pada nama
+
 $(".navbar-brand").on('click', function(event) {
-    // Misalnya, untuk navigasi ke bagian about pada halaman
+   
     let target = "#about";
 
-    // Menggunakan animasi smooth scrolling ke bagian about
+ 
     let offsetTop = $(target)?.offset()?.top ?? 0;
     $('html, body').animate({
         scrollTop: offsetTop
     }, 800);
 
-    // Tambahkan tindakan lainnya di sini
+   
     alert("Anda mengklik nama Saya dan Anda akan diarahkan ke bagian About!");
 });
 
-// Contoh fungsi untuk menghitung panjang string
+
 function hitungPanjangString(input: string): number {
     return input.length;
 }
+
+
+window.addEventListener('load', () => {
+  setTimeout(() => {
+    const loadingScreen = document.getElementById('loading-screen');
+    if (loadingScreen) {
+      loadingScreen.style.display = 'none';
+    }
+  }, 2000);
+});
